727d5611848db9378514a7f71e2319b523556cbb
Eric Fischer <enf@pobox.com>
2019-10-08 12:28:49 -0700

    Tolerate a non-JSON response to "tilesets update-recipe"

--- tilesets/scripts/cli.py
+++ tilesets/scripts/cli.py
@@ -281,7 +281,10 @@ def update_recipe(tileset, recipe, token=None, indent=None):
         r = requests.patch(url, json=recipe_json)
         if r.status_code == 201:
             click.echo("Updated recipe.", err=True)
-            click.echo(json.dumps(r.json(), indent=indent))
+            try:
+                click.echo(json.dumps(r.json(), indent=indent))
+            except:
+                click.echo(r.text)
         else:
             raise errors.TilesetsError(r.text)
 
