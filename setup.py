b48fc6de6b514c3b3fdb26667433653c26f2305a
gerdie <maria.moy@mapbox.com>
2020-04-01 11:40:34 -0700

    rename to mapbox-tilesets

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 
 setup(
-    name="tilesets-cli",
+    name="mapbox-tilesets",
     version=__version__,
     description=u"CLI for interacting with and preparing data for the Tilesets API",
     long_description=long_description,
