cf3a7986fc135076190cc5e8d71dea706a44a566
dianeschulze <diane.e.schulze@gmail.com>
2020-06-02 13:28:46 -0700

    lint

--- tests/test_version_documentation.py
+++ tests/test_version_documentation.py
@@ -4,4 +4,4 @@ from mapbox_tilesets import __version__
 def test_versions():
     mod_version = __version__
     with open("./CHANGELOG.md") as src:
-        assert len([l in l for l in src if mod_version in l]) == 1
+        assert len([line in line for line in src if mod_version in line]) == 1
