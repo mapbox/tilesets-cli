8e890058958ad263cdd298a38561790d391cf40b
dianeschulze <diane.e.schulze@gmail.com>
2020-06-02 16:07:45 -0700

    Don't print response text on update-recipe

--- mapbox_tilesets/scripts/cli.py
+++ mapbox_tilesets/scripts/cli.py
@@ -281,7 +281,6 @@ def update_recipe(tileset, recipe, token=None, indent=None):
         r = requests.patch(url, json=recipe_json)
         if r.status_code == 201 or r.status_code == 204:
             click.echo("Updated recipe.", err=True)
-            click.echo(r.text)
         else:
             raise errors.TilesetsError(r.text)
 
