ad89ea9827c4d260eb25b00f8cbc914dcc196b6e
mapsam <matthews.sam@gmail.com>
2020-06-03 12:01:34 -0700

    assert TilesetsError

--- tests/test_cli_delete.py
+++ tests/test_cli_delete.py
@@ -5,11 +5,13 @@ from click.testing import CliRunner
 from unittest import mock
 
 from mapbox_tilesets.scripts.cli import delete
+from mapbox_tilesets.errors import TilesetsError
 
 
 class MockResponse:
     def __init__(self, mock_json, status_code):
         self.text = json.dumps(mock_json)
+        print(self.text)
         self._json = mock_json
         self.status_code = status_code
 
@@ -45,3 +47,20 @@ def test_cli_delete_force(mock_request_delete):
     )
     assert result.exit_code == 0
     assert result.output == ""
+
+
+@pytest.mark.usefixtures("token_environ")
+@mock.patch("requests.delete")
+def test_cli_delete_fail(mock_request_delete):
+    runner = CliRunner()
+
+    # sends expected request
+    mock_request_delete.return_value = MockResponse(
+        {"message": "uh oh"}, status_code=422
+    )
+    result = runner.invoke(delete, ["test.id", "--force"])
+    mock_request_delete.assert_called_with(
+        "https://api.mapbox.com/tilesets/v1/test.id?access_token=fake-token"
+    )
+    assert result.exit_code == 1
+    assert isinstance(result.exception, TilesetsError)
