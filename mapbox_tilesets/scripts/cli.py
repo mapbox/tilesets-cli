50996cd7cd75859a268f5bff4becbabd7c4668e4
mapsam <matthews.sam@gmail.com>
2020-06-02 17:59:47 -0700

    fix flake8

--- mapbox_tilesets/scripts/cli.py
+++ mapbox_tilesets/scripts/cli.py
@@ -118,6 +118,65 @@ def publish(tileset, token=None, indent=None):
         raise errors.TilesetsError(f"{r.text}")
 
 
+@cli.command("update")
+@click.argument("tileset", required=True, type=str)
+@click.option("--token", "-t", required=False, type=str, help="Mapbox access token")
+@click.option("--indent", type=int, default=None, help="Indent for JSON output")
+@click.option("--name", "-n", required=False, type=str, help="name of the tileset")
+@click.option(
+    "--description", "-d", required=False, type=str, help="description of the tileset"
+)
+@click.option(
+    "--privacy",
+    "-p",
+    required=False,
+    type=click.Choice(["public", "private"]),
+    help="set the tileset privacy options",
+)
+@click.option(
+    "--attribution",
+    required=False,
+    type=str,
+    help="attribution for the tileset in the form of a JSON string - Array<Object<text,link>>",
+)
+def update(
+    tileset,
+    token=None,
+    indent=None,
+    name=None,
+    description=None,
+    privacy=None,
+    attribution=None,
+):
+    """Update a tileset's information.
+
+    tilesets update <tileset_id>
+    """
+    mapbox_api = _get_api()
+    mapbox_token = _get_token(token)
+    url = "{0}/tilesets/v1/{1}?access_token={2}".format(
+        mapbox_api, tileset, mapbox_token
+    )
+    body = {}
+    if name:
+        body["name"] = name
+    if description:
+        body["description"] = description
+    if privacy:
+        body["private"] = True if privacy == "private" else False
+    if attribution:
+        try:
+            body["attribution"] = json.loads(attribution)
+        except:
+            click.echo("Unable to parse attribution JSON")
+            click.exit(1)
+
+    r = requests.patch(url, json=body)
+
+    if r.status_code != 204:
+        raise errors.TilesetsError(f"{r.text}")
+
+
 @cli.command("status")
 @click.argument("tileset", required=True, type=str)
 @click.option("--token", "-t", required=False, type=str, help="Mapbox access token")
@@ -292,7 +351,7 @@ def validate_source(features):
     """Validate your source file.
     $ tilesets validate-source <path/to/your/src/file>
     """
-    click.echo(f"Validating features", err=True)
+    click.echo("Validating features", err=True)
 
     for feature in features:
         utils.validate_geojson(feature)
---
+++ tests/test_cli_update.py
@@ -0,0 +1,80 @@
+import json
+import pytest
+
+from click.testing import CliRunner
+from unittest import mock
+
+from mapbox_tilesets.scripts.cli import update
+
+
+class MockResponse:
+    def __init__(self, mock_json, status_code):
+        self.text = json.dumps(mock_json)
+        self._json = mock_json
+        self.status_code = status_code
+
+    def MockResponse(self):
+        return self
+
+    def json(self):
+        return self._json
+
+
+@pytest.mark.usefixtures("token_environ")
+@mock.patch("requests.patch")
+def test_cli_patch(mock_request_patch):
+    runner = CliRunner()
+
+    # sends expected request
+    mock_request_patch.return_value = MockResponse("", 204)
+    result = runner.invoke(
+        update,
+        [
+            "test.id",
+            "--name=hola",
+            "--description=hello world",
+            "--privacy=private",
+            '--attribution=[{"text":"natural earth data","link":"https://naturalearthdata.com"}]',
+        ],
+    )
+
+    mock_request_patch.assert_called_with(
+        "https://api.mapbox.com/tilesets/v1/test.id?access_token=fake-token",
+        json={
+            "name": "hola",
+            "description": "hello world",
+            "private": True,
+            "attribution": [
+                {"text": "natural earth data", "link": "https://naturalearthdata.com"}
+            ],
+        },
+    )
+    assert result.exit_code == 0
+    assert result.output == ""
+
+
+@pytest.mark.usefixtures("token_environ")
+@mock.patch("requests.patch")
+def test_cli_patch_no_options(mock_request_patch):
+    runner = CliRunner()
+
+    # sends expected request
+    mock_request_patch.return_value = MockResponse("", 204)
+    result = runner.invoke(update, ["test.id"])
+
+    mock_request_patch.assert_called_with(
+        "https://api.mapbox.com/tilesets/v1/test.id?access_token=fake-token", json={}
+    )
+    assert result.exit_code == 0
+    assert result.output == ""
+
+
+@pytest.mark.usefixtures("token_environ")
+def test_cli_patch_invalid_json():
+    runner = CliRunner()
+
+    # send invalid json request
+    result = runner.invoke(update, ["test.id", "--attribution=invalid json"])
+
+    assert result.exit_code == 1
+    assert "Unable to parse attribution JSON" in result.output
