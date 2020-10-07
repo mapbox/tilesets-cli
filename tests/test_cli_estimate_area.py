601d3f2b1f5d136553a4f719e65a930de430c451
allisonzheng <allison.zheng@mapbox.com>
2020-10-06 17:02:49 -0700

    added input tests

--- tests/test_cli_estimate_area.py
+++ tests/test_cli_estimate_area.py
@@ -1,8 +1,42 @@
-# from click.testing import CliRunner
-# import json
-# import os
-# from unittest import mock
+from click.testing import CliRunner
 
-# import pytest
+from mapbox_tilesets.scripts.cli import estimate_area
 
-# from mapbox_tilesets.scripts.cli import estimate_area
+# this just keeps running indefinetly
+# def test_cli_estimate_area_no_features():
+#     runner = CliRunner()
+#     invalidated_result = runner.invoke(
+#         estimate_area,
+#         ["features", " ", "--precision", "10m"],
+#     )
+#     assert invalidated_result.exit_code == 1
+
+
+def test_cli_estimate_area_no_precision():
+    runner = CliRunner()
+    invalidated_result = runner.invoke(
+        estimate_area,
+        ["tests/fixtures/valid.ldgeojson"],
+    )
+    assert invalidated_result.exit_code == 2
+    assert "Missing option '--precision' / '-p'" in invalidated_result.output
+
+
+def test_cli_estimate_area_invalid_precision():
+    runner = CliRunner()
+    invalidated_result = runner.invoke(
+        estimate_area,
+        ["tests/fixtures/valid.ldgeojson", "-p", "100m"],
+    )
+    assert invalidated_result.exit_code == 2
+    assert "Invalid value for '--precision' / '-p'" in invalidated_result.output
+
+
+# TODO : add output validation
+def test_cli_estimate_area_valid_features_files_and_precision():
+    runner = CliRunner()
+    validated_result = runner.invoke(
+        estimate_area,
+        ["tests/fixtures/valid.ldgeojson", "-p", "10m"],
+    )
+    assert validated_result.exit_code == 0
