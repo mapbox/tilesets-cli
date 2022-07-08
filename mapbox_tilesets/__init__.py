42612f7a7d5b4d170e6ac422008d018586b0422c
Shrawani Pampatwar <shrawani.pampatwar@mapbox.com>
2022-07-08 09:51:43 -0700

    fix pre-commit error

--- mapbox_tilesets/__init__.py
+++ mapbox_tilesets/__init__.py
@@ -1,7 +1,3 @@
 """mapbox_tilesets package"""
-import warnings
 
 __version__ = "1.7.4"
-
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
