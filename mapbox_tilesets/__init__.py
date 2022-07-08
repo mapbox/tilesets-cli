920ce1c6ed01c2a26a72ea5d160addc65819f259
Shrawani Pampatwar <shrawani.pampatwar@mapbox.com>
2022-07-07 17:09:01 -0700

    fix indentation

--- mapbox_tilesets/__init__.py
+++ mapbox_tilesets/__init__.py
@@ -1,7 +1,7 @@
 """mapbox_tilesets package"""
+import warnings
 
 __version__ = "1.7.4"
 
-import warnings
-    warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-    warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
