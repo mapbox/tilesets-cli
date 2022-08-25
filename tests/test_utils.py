71912be09906c195b220bd0625e351f9a4f6f73b
Sofia Paganin <31298051+sofita@users.noreply.github.com>
2022-08-25 11:34:55 -0700

    Update tests/test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -101,7 +101,7 @@ def test_geojson_validate_closed_ring():
 def test_geojson_validate_closed_ring_correct_winding_order():
     geometry = {
         "type": "Polygon",
-        "coordinates": [[[0, 0], [1, 0], [1, 1], [0, 1, [0, 0]]],
+        "coordinates": [[[0, 0], [1, 0], [1, 1], [0, 1], [0, 0]]],
     }
     assert geojson_validate(2, geometry) is None
 
