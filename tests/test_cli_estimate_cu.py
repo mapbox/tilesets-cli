1f247d7a6a5c47cd7c1070c64ed4c1ae9f1804fe
nicklvsa <nicholas.gerancher@mapbox.com>
2024-10-23 10:40:54 -0400

    zoom test

--- tests/test_cli_estimate_cu.py
+++ tests/test_cli_estimate_cu.py
@@ -93,3 +93,35 @@ def test_cli_estimate_cu_tileset_with_sources(
         result.output
         == f"\nEstimated CUs for '{tileset_id}': {msg['cu']}. To publish your tileset, run 'tilesets publish'.\n"
     )
+
+@pytest.mark.usefixtures("token_environ")
+@pytest.mark.usefixtures("api_environ")
+@mock.patch("requests.Session.get")
+@mock.patch("glob.glob")
+def test_cli_estimate_cu_tileset_with_zoom_overrides(
+    mock_glob, mock_request_get, MockResponse
+):
+    runner = CliRunner()
+
+    tileset_id = "my.tileset"
+    msg = {"cu": "5"}
+
+    mock_request_get.return_value = MockResponse(msg)
+    mock_glob.return_value = ["myfile.grib2"]
+    result = runner.invoke(estimate_cu, [tileset_id, "-s", "/my/sources/*.grib2", "--minzoom", 1, "--maxzoom", 6])
+    mock_request_get.assert_called_with(
+        f"https://api.mapbox.com/tilesets/v1/{tileset_id}/estimate",
+        params={
+            "minzoom": 1,
+            "maxzoom": 6,
+            "filesize": 0,
+            "band_count": 15,
+            "access_token": "pk.eyJ1IjoidGVzdC11c2VyIn0K",
+        },
+    )
+
+    assert result.exit_code == 0
+    assert (
+        result.output
+        == f"\nEstimated CUs for '{tileset_id}': {msg['cu']}. To publish your tileset, run 'tilesets publish'.\n"
+    )
