7fd940279dfb66e9010f27e0eebd9eace4d1c4db
jqtrde <git@jqtrde.com>
2025-11-20 10:19:43 -0500

    Adjust versioning mechanics

--- mapbox_tilesets/__init__.py
+++ mapbox_tilesets/__init__.py
@@ -5,4 +5,5 @@ from importlib.metadata import PackageNotFoundError, version as _pkg_version
 try:
     __version__ = _pkg_version("mapbox-tilesets")
 except PackageNotFoundError:
-    __version__ = "0.0.0"
+    # Fallback for development mode
+    __version__ = "0.0.0.dev"
