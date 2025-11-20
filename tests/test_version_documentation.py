7f7d362d21456b11273e445bcd1f2a07a90c21d9
jqtrde <git@jqtrde.com>
2025-11-19 22:23:33 -0500

    Remove version test
    We'll do this a little differently soon.

--- tests/test_version_documentation.py
+++
@@ -1,7 +0,0 @@
-from mapbox_tilesets import __version__
-
-
-def test_versions():
-    mod_version = __version__
-    with open("./CHANGELOG.md") as src:
-        assert len([line in line for line in src if mod_version in line]) == 1
