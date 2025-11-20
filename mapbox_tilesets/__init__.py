6280f299d48ea359c22961e1dbc0c7b8a3ee2678
jqtrde <git@jqtrde.com>
2025-11-19 21:47:24 -0500

    Pull version from pyproject.toml

--- mapbox_tilesets/__init__.py
+++ mapbox_tilesets/__init__.py
@@ -1,3 +1,8 @@
 """mapbox_tilesets package"""
 
-__version__ = "1.14.0"
+from importlib.metadata import PackageNotFoundError, version as _pkg_version
+
+try:
+    __version__ = _pkg_version("mapbox-tilesets")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
